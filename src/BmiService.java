d595babc2998e74da7446e2cafd5d69346e7513d
Igor Smirnov <inextgen@mail.ru>
2023-02-12 11:59:23 +0300

    Main.java code update

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
     // ИМТ = m/h2, где: m — масса тела в килограммах, h — рост в метрах
-    public double calculate (double m, double h){
+    public double calculate(double m, double h) {
         double bmi;
-        bmi = m /(h * h);
+        bmi = m / (h * h);
         return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-
-        double mass = 98;  //kilograms
-        double height = 1.87; //meters
-        double index = (int)service.calculate (mass, height);
-
-        System.out.println("Индекс массы тела равен " + index);
+        // метод ([масса тела в кг], [рост в метрах])
+        System.out.println("Индекс массы тела равен " + (int) service.calculate(98, 1.87));
     }
 }
